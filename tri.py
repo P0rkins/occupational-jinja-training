4831535efe73081deed86cf112e7ad6469e21d63
Laurentius912 <lvargas@stuy.edu>
2016-09-22 14:50:17 -0400

    worked a bit xdd

--- tri.py
+++ tri.py
@@ -1,17 +1,41 @@
 #Big L is the coolest cat in the village
 
 from flask import Flask, render_template
-app = Flask(__name__) #create Flask object
+app = Flask(__name__) #create Flask objec
+
+import random
+
+inStream = open('occupations.csv','r')
+occupation = inStream.readlines()
+inStream.close()
 
 @app.route("/") #assign following fxn to run when root route requested
 def hello_world():
-    return "No hablo queso!"
+    return "Big L is the coolest cat in the village. Be like him."
 
-khal = [0,1,1,2,3,5,8]
+occupations = {}
 
-@app.route("/my_foist_template")
-def test_tmplt():
-    return render_template( 'model_tmplt.html', foo="foooood", collection=khal)
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def store(string):
+    for data in occupation:
+        stripped = data.rstrip() #stripped data of /n
+        temp = stripped.split(',')
+        if is_number(temp[1]):
+            dictionary[temp[0]] = float(temp[1])
+
+store(occupations)
+occupations.pop('Total', None)
+
+
+@app.route("/occupations")
+def occupy():
+    return render_template( 'model.html', collection = occupations)
     
 if __name__ == "__main__":
     #enable debugging, auto-restarting of server when this file is modified
